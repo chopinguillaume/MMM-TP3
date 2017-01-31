e2dd9b732e1a982c3cbffa04bae0f4a4a14b98c0
Guillaume Chopin <guillaume.chopin@etudiant.univ-rennes1.fr>
2017-02-01 00:06:45 +0100

    list + web fragments working for tablets

--- app/src/main/java/com/example/guillaume/tp3_mmm/MainActivity.java
+++ app/src/main/java/com/example/guillaume/tp3_mmm/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.guillaume.tp3_mmm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.example.guillaume.tp3_mmm.fragments.DetailFragment;
 import com.example.guillaume.tp3_mmm.fragments.RegionListFragment;
@@ -15,7 +14,6 @@ public class MainActivity extends AppCompatActivity implements RegionListFragmen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         if (findViewById(R.id.frame_region_list) != null) {
             if (savedInstanceState != null) {
@@ -31,13 +29,8 @@ public class MainActivity extends AppCompatActivity implements RegionListFragmen
     @Override
     public void onListFragmentInteraction(Region item) {
         if (findViewById(R.id.frame_region_detail) != null) {
-            Region region = new Region("Alsace");
-            region.setUrl("http://technoresto.org/vdf/alsace/index.html");
             DetailFragment detailFragment = DetailFragment.newInstance(item);
-            Log.d("Debug", item.getName());
-            Log.d("Debug", item.getUrl());
-            getSupportFragmentManager().beginTransaction().add(R.id.frame_region_detail, detailFragment).commit();
-
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_region_detail, detailFragment).addToBackStack(null).commit();
         } else {
             Intent i = new Intent(getApplicationContext(), WebViewActivity.class);
             i.putExtra("region", item);
